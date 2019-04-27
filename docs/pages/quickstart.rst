98849d6276b9f4b6df565f8a9e9d691a0c5d1766
Robert Graham <rpgraham84@gmail.com>
2019-04-27 15:26:51 -0400

    Update docs

--- docs/pages/quickstart.rst
+++ docs/pages/quickstart.rst
@@ -15,7 +15,7 @@ be the default for sites without a favicon). That's easy:
 
 .. code-block::
 
-    http://favi.codes
+    https://favi.codes
 
 This will return a transparent, 256x256 PNG.
 
@@ -33,7 +33,7 @@ because the letters look too cramped when there are 3 or more).
 
 .. code-block::
 
-    http://favi.codes/JS
+    https://favi.codes/JS
 
 This will return a favicon with "JS" in black letters on a transparent background, like this:
 
@@ -43,7 +43,67 @@ You can also do background colors:
 
 .. code-block::
 
-    http://favi.codes/JS?bgcolor=yellow
+    https://favi.codes/JS?bgcolor=gold
 
-.. image:: https://favi.codes/JS?bgcolor=yellow
+.. image:: https://favi.codes/JS?bgcolor=gold
+
+It you're familiar with the Javascript logo, you may recognize where this is heading. 
+Let's see how close we can get it. Maybe make the text bold, and offset the X and Y
+to put the "JS" in the bottom right corner
+
+.. code-block::
+
+    https://favi.codes/JS?&bgcolor=gold&y=35&x=65&fontsize=128&style=bold
+
+.. image:: https://favi.codes/JS?&bgcolor=gold&y=35&x=65&fontsize=128&style=bold
+
+Now, if you're *really* familiar with the JavaScript logo, you may be irked that the
+J extends below the line. This is because the only fonts supported by Favicodes at the moment are
+Google Noto Sans and FontAwesome, and that's just how capital J's look in both of those fonts. 
+
+There is a workaround for this logo, since it is included with FontAwesome Free.
+To use FontAwesome icons in your favicode, you have 2 options. The first option is if you know the
+proper name of the icon, such as "paper-plane" or "js", you can use it the url after
+the path, /fa/, like this:
+
+.. code-block::
+
+    https://favi.codes/fa/js
+
+.. image:: https://favi.codes/fa/js
+
+Obviously, this doesn't match either. After some fiddling, I came up with this:
+
+.. code-block::
+
+    https://favi.codes/fa/js?color=gold&bgcolor=black&fontsize=256&y=-8&size=224
+
+.. image:: https://favi.codes/fa/js?color=gold&bgcolor=black&fontsize=256&y=-8&size=224
+
+Alternatively, that same FontAwesome icon can be referenced by it's Unicode code point instead, so long
+as FontAwesome is specified as the font. And, because the font is being specified, you'll have
+to specify the style for this code point as well. `The code point is f3b8 and the style is 'brands' for this icon <https://fontawesome.com/icons/js?style=brands>`_. Like this:
+
+.. code-block::
+
+    https://favi.codes/f3b8?color=gold&bgcolor=black&fontsize=256&y=-8&size=224&font=fontawesome&style=brands
+
+
+Advanced usage
+==============
+
+We just touched on Unicode code points in the URL. You can actually specify any character input this way, 
+including UTF-32LE characters, although I doubt any of the fonts supported have glyphs in that range. Any input
+past the root slash '/' that is longer than 2 characters (because Favicodes can only have 2 characters) is expected
+to be valid hex representing a character in the font. 
+
+So, suppose you wanted to make Slashdot's favicon. You can't use '/' or '.' in the URL bar, and 
+URL encoding wont work for the slash, because to webserver will eat the slash. All you need to
+know is the Unicode values for slash and dot, 002F and 002E:
+
+.. code-block::
+
+    https://favi.codes/02f/02e?color=white&style=extrabolditalic&x=10&y=-25&bgcolor=104f4e&fontsize=224
+
+.. image:: https://favi.codes/02f/02e?color=white&style=extrabolditalic&x=10&y=-25&bgcolor=104f4e&fontsize=224
 
--- validation.py
+++ validation.py
@@ -29,39 +29,6 @@ def validate(characters, icon_name, request):
     }
     errors = defaultdict(list)
 
-    def validate_font_and_style():
-        font = request.args.get("font", defaults["font"])
-        style = request.args.get("style", defaults["style"])
-        try:
-            fontnames = set(f[0] for f in fonts)
-            assert font in fontnames, f"Font must be one of {fontnames}"
-        except (ValueError, AssertionError) as e:
-            errors["font"].append(str(e))
-            return
-        else:
-            defaults["font"] = font.lower()
-
-        try:
-            valid_styles = set(k[1] for k, v in fonts.items() if font == k[0])
-            assert (
-                style in valid_styles
-            ), f"Style must be one of {valid_styles} if font is {font}"
-        except (ValueError, AssertionError) as e:
-            errors["style"].append(str(e))
-        else:
-            defaults["style"] = style
-
-    def validate_icon():
-        style = request.args.get("style")
-        if icon_name:
-            icon = get_icon_by_name(icon_name)
-            if not icon:
-                errors["icon_name"].append(f"'{icon_name}' is not a valid icon name")
-            else:
-                if style not in icon["styles"]:
-                    defaults["style"] = icon["styles"][0]
-                defaults["input"] += chr(int(icon["unicode"], 16))
-
     def validate_characters():
         for char in characters:
             if char:
@@ -77,45 +44,6 @@ def validate(characters, icon_name, request):
                     defaults["input"] += char
                     if len(defaults["input"]) == 2:
                         break
-
-    def validate_format():
-        frmat = request.args.get("format")
-        valid_formats = ["png", "ico"]
-        if frmat:
-            try:
-                assert frmat in valid_formats, "format must be one of ['png', 'ico']"
-            except AssertionError as e:
-                errors["input"].append(str(e))
-
-    def validate_lte_256(params=["size", "fontsize"]):
-        param = params.pop()
-        size = request.args.get(param)
-        if size:
-            try:
-                size = abs(int(size))
-                assert size <= 256, "Size must be <= 256"
-            except (ValueError, AssertionError) as e:
-                errors[param].append(str(e))
-            else:
-                defaults[param] = size
-
-        if params:
-            validate_lte_256(params)
-
-    def validate_offset(params=["x", "y"]):
-        param = params.pop()
-        value = request.args.get(param)
-        if value:
-            try:
-                value = int(value)
-                assert -128 <= value <= 128, "value must satisfy -128 <= value <= 128"
-            except (ValueError, AssertionError) as e:
-                errors[param].append(str(e))
-            else:
-                defaults[param] = value
-
-        if params:
-            validate_offset(params)
 
     def validate_colors(params=["color", "bgcolor"]):
         param = params.pop()
@@ -147,6 +75,80 @@ def validate(characters, icon_name, request):
 
         if params:
             validate_colors(params)
+
+    # These functions run in alphabetical order, which is important for
+    # defaults['style'] to be set properly
+    def validate_font_and_style():
+        font = request.args.get("font", defaults["font"])
+        style = request.args.get("style", defaults["style"])
+        try:
+            fontnames = set(f[0] for f in fonts)
+            assert font in fontnames, f"Font must be one of {fontnames}"
+        except (ValueError, AssertionError) as e:
+            errors["font"].append(str(e))
+            return
+        else:
+            defaults["font"] = font.lower()
+
+        try:
+            valid_styles = set(k[1] for k, v in fonts.items() if font == k[0])
+            assert (
+                style in valid_styles
+            ), f"Style must be one of {valid_styles} if font is {font}"
+        except (ValueError, AssertionError) as e:
+            errors["style"].append(str(e))
+        else:
+            defaults["style"] = style
+
+    def validate_format():
+        frmat = request.args.get("format")
+        valid_formats = ["png", "ico"]
+        if frmat:
+            try:
+                assert frmat in valid_formats, "format must be one of ['png', 'ico']"
+            except AssertionError as e:
+                errors["input"].append(str(e))
+
+    def validate_icon():
+        style = request.args.get("style")
+        if icon_name:
+            icon = get_icon_by_name(icon_name)
+            if not icon:
+                errors["icon_name"].append(f"'{icon_name}' is not a valid icon name")
+            else:
+                if style not in icon["styles"]:
+                    defaults["style"] = icon["styles"][0]
+                defaults["input"] += chr(int(icon["unicode"], 16))
+
+    def validate_lte_256(params=["size", "fontsize"]):
+        param = params.pop()
+        size = request.args.get(param)
+        if size:
+            try:
+                size = abs(int(size))
+                assert size <= 256, "Size must be <= 256"
+            except (ValueError, AssertionError) as e:
+                errors[param].append(str(e))
+            else:
+                defaults[param] = size
+
+        if params:
+            validate_lte_256(params)
+
+    def validate_offset(params=["x", "y"]):
+        param = params.pop()
+        value = request.args.get(param)
+        if value:
+            try:
+                value = int(value)
+                assert -128 <= value <= 128, "value must satisfy -128 <= value <= 128"
+            except (ValueError, AssertionError) as e:
+                errors[param].append(str(e))
+            else:
+                defaults[param] = value
+
+        if params:
+            validate_offset(params)
 
     for name, validator in sorted(locals().items()):
         if name.startswith("validate_") and callable(validator):
